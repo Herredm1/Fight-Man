63710bc972c98bdabedc7a4f8217e99eda0a8cab
Herredm1 <david.m.herrera1989@gmail.com>
2023-03-10 15:46:51 +0700

    added save function to main code

--- Save.py
+++ Save.py
@@ -1,4 +1,5 @@
 import shelve
+import Player
 
 class Save():
     def __init__(self, fileName):
@@ -10,7 +11,23 @@ class Save():
             for k, v in dict.items():
                 db.update({k:v})
             
-    def get_option(self, dict:dict):
+    def get_option(self, player:object):
         with shelve.open(self.fileName) as db:
-            dict.update(db)
-            return dict
+            print(db['name'])
+            player.name = db['name']
+            player.lvl = db['level']
+            player.str = db['STR']
+            player.denf = db['DEF']
+            player.exp = db['EXP']
+            player.status = db['status']
+            
+                
+                
+                
+                
+                 
+               
+                
+                
+                
+                
--- test.py
+++ test.py
@@ -2,28 +2,67 @@ from Player import Player
 from mobGenerator import monsterPicker
 from combat import combat
 from os import system as sys
+from Save import Save
 
 
-# Player Template       
-player = Player('temp', 1 , 0, 0, 0, 'new')
-
+# Player Template
+try:
+    player = Player("temp", 1, 0, 0, 0, "new")
+    Save('save.db').get_option(player)
+    
+except:
+    pass
 # For future Inventory update
 inventory = []
 
 # List of monster names that random pulls from. Will add this to the monsterPicker function later
-monsterNames = ['Kevin','Kelly','Mike','Daile', 'Marcile', 'Germain', 'Row', 'Gayla', 'Bobbe', 'Patrice', 'Lucia', 'Sorcha', 'Angela', 'Elvira', 'Sandie', 'Hedvige', 'Millie', 'Janina', 'Tasia', 'Daisi', 'Delia', 'Kessiah', 'Hyacinthia', 'Sybil', 'Zoe', 'Thia', 'Janaye', 'Dorothee', 'Trisha', 'Merilee', 'Zola', 'Anthe', 'Kellsie']
+monsterNames = [
+    "Kevin",
+    "Kelly",
+    "Mike",
+    "Daile",
+    "Marcile",
+    "Germain",
+    "Row",
+    "Gayla",
+    "Bobbe",
+    "Patrice",
+    "Lucia",
+    "Sorcha",
+    "Angela",
+    "Elvira",
+    "Sandie",
+    "Hedvige",
+    "Millie",
+    "Janina",
+    "Tasia",
+    "Daisi",
+    "Delia",
+    "Kessiah",
+    "Hyacinthia",
+    "Sybil",
+    "Zoe",
+    "Thia",
+    "Janaye",
+    "Dorothee",
+    "Trisha",
+    "Merilee",
+    "Zola",
+    "Anthe",
+    "Kellsie",
+]
 
 # Monster vairables. Will move to the monsterPicker function later.
-levels =  [player.lvl - 1 , player.lvl, player.lvl + 1]
-power = [player.str - 1 , player.str, player.str + 1]
-defense = [player.denf - 1 , player.denf, player.denf + 1]
+levels = [player.lvl - 1, player.lvl, player.lvl + 1]
+power = [player.str - 1, player.str, player.str + 1]
+defense = [player.denf - 1, player.denf, player.denf + 1]
 
 # Temporary Main function
-def main(player:object):
+def main(player: object):
 
     while True:
         points = 15
-        if player.status == 'new':
+        if player.status == "new":
             playerName = input("What is your name Warrior: ")
             while points > 0:
                 print(f"You have {points} points to spend")
@@ -37,7 +76,7 @@ def main(player:object):
                     elif points <= 0:
                         break
                     print(f"{points} remaining")
-                    protect = int(input('How much Defense would you like: '))
+                    protect = int(input("How much Defense would you like: "))
                     points -= protect
                     if protect == 15:
                         print("At least one point must be spent on Attack")
@@ -46,46 +85,63 @@ def main(player:object):
                         print("You used too many points. Restarting skills selection.")
                         might = 0
                         protect = 0
-                        points= 15
+                        points = 15
                         continue
                     elif points == 0:
                         break
                 except:
                     print("Invalid selection! Try again")
                     continue
-            status = 'ready'
+            status = "ready"
             player = Player(playerName, 1, might, protect, 0, status)
-        elif player.status == 'ready':
-            sys('cls')
-            while player.status == 'ready':
-                try:    
-                    playGame = int(input(f"""
+            saveTemp= {
+                'name': player.name,
+                'level': player.lvl,
+                'STR': player.str,
+                'DEF': player.denf,
+                'EXP': player.exp,
+                'status': player.status
+            }
+            Save('save.db').set_option(dict=saveTemp)
+        elif player.status == "ready":
+            sys("cls")
+            while player.status == "ready":
+                try:
+                    playGame = int(
+                        input(
+                            f"""
                                     What would you like to do
                                          
                                     {player}
                                     
                                             1. Fight
-                                            2. Save
+                                            2. Save (Feature not yet added)
                                             3. Exit
                                             
-                                        selection: """))
+                                        selection: """
+                        )
+                    )
                     if playGame == 1:
-                        picked_monster = monsterPicker(monsterNames, levels, power, defense, player)
+                        picked_monster = monsterPicker(
+                            monsterNames, levels, power, defense, player
+                        )
                         combat(player, picked_monster)
                     elif playGame == 2:
                         sys.exit()
-                    
+
                 except:
                     print("Invalid selection! Try again")
                     continue
                 if player.hp <= 0:
-                    player.status = 'defeat'
+                    player.status = "defeat"
                 else:
-                    player.status = 'victory'                
-        elif player.status == 'victory':
-                sys('cls')
-                try:
-                    playGame = int(input(f"""
+                    player.status = "victory"
+        elif player.status == "victory":
+            sys("cls")
+            try:
+                playGame = int(
+                    input(
+                        f"""
                                          That was amazaing! You defeated {picked_monster.name}. What would you like to do
                                         
                                             {player}
@@ -94,36 +150,46 @@ def main(player:object):
                                                     2. Heal
                                                     3. Exit 
                                                     
-                                                selection: """))
-                    if playGame == 1:
-                        picked_monster = monsterPicker(monsterNames, levels, power, defense, player)
-                        combat(player, picked_monster)
-                    if playGame == 2:
-                        player.heal()
-                        print(player.hp)
-                        player.status = 'ready'
-                except:
-                    print("Invalid selection! Try again")
-                    continue
-                
-                if player.hp <= 0:
-                    player.status = 'defeat'
-                
-        elif player.status == 'defeat':
-            sys('cls')
+                                                selection: """
+                    )
+                )
+                if playGame == 1:
+                    picked_monster = monsterPicker(
+                        monsterNames, levels, power, defense, player
+                    )
+                    combat(player, picked_monster)
+                if playGame == 2:
+                    player.heal()
+                    print(player.hp)
+                    player.status = "ready"
+            except:
+                print("Invalid selection! Try again")
+                continue
+
+            if player.hp <= 0:
+                player.status = "defeat"
+
+        elif player.status == "defeat":
+            sys("cls")
             try:
-                playGame = int(input(f'''Looks like {picked_monster.name} whoooped your ass. What are you going to do about that?
+                playGame = int(
+                    input(
+                        f"""Looks like {picked_monster.name} whoooped your ass. What are you going to do about that?
                                                     
                                                     1.Heal
                                                     2.Exit
                                                     
-                                                selection: '''))
+                                                selection: """
+                    )
+                )
                 if playGame == 1:
                     player.heal()
-                    player.status = 'ready'
+                    player.status = "ready"
                 elif playGame == 2:
                     sys.exit()
             except:
                 print("Invalid selection! Try again")
                 continue
+
+
 main(player)
