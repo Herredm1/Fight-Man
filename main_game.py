cbda8df921465537cb80624f043e3327184da5ce
Herredm1 <david.m.herrera1989@gmail.com>
2023-03-15 02:35:47 +0700

    Fixed bugs, edit print layouts

--- main_game.py
+++ main_game.py
@@ -5,51 +5,56 @@ from os import system as sys
 from globalFunctions import monsterPicker, combat
 import time
 
+
 def main(playerCard:Player, monster:Monster):
-    
     while True:
+        pINFO = [playerCard.name, playerCard.lvl, playerCard.hp,playerCard.baseHP, playerCard.atkPower, playerCard.blkPower, playerCard.str, playerCard.denf,'Fight', 'Heal', 'Exit to Main Menu', monster.name, playerCard.exp, playerCard.baseEXP]
         sys('cls')
         if playerCard.status == "ready":
-            print(playerCard)
-            playGame = input(f"""                             What would you like to do
-                                
-                                        1. Fight
-                                        2. Heal
-                                        3. Exit to Main Menu
-                                        
-                                        selection: """)
+            banner = '''                                     What would you like to do?
+            {0}
+            LVL : {1}
+            HP  : {2}/{3}                                1.{8}
+            ATK : {4}                                   2. {10}
+            BLK : {5}                                   
+            STR : {6}
+            DFN : {7}
+            XP  : {12}/{13}
+                                                                                    '''.format(*pINFO)
+            print(banner)
+            playGame = input('Selection: ')
             
             try:
                 if int(playGame) == 1:
                     monster = monsterPicker(player=playerCard)
                     playerCard, monster = combat(playerCard, monster)
-                elif int(playGame) == 2:
-                    playerCard.heal_and_status()
-                    sys('cls')
                 elif int(playGame) == 3:
                     Save(playerCard.name).save_state(playerCard, monster)
             except ValueError:
                 print("Invalid selection. Please try again")
 
         elif playerCard.status == "victory":
-            print(playerCard)
-            playGame =input(
-                        f"""
-                                        That was amazaing! You defeated {monster.name}. What would you like to do
-                                        
-                                            {playerCard}
-                                        
-                                                    1. Fight
-                                                    2. Heal
-                                                    3. Exit to Main Menu
-                                                    
-                                                selection: """
-                    )
+            sys('cls')
+            banner = '''        That was an Epic fight! {11} didn't even stand a chance. What would you like to do?
+        
+        
+        {0}
+        LVL : {1}                                    1. {8}
+        HP  : {2}/{3}                                2. {9}
+        ATK : {4}                                   3. {10}
+        BLK : {5}                                    
+        STR : {6}
+        DFN : {7}
+        XP  : {12}/{13}
+            
+                                                                                        '''.format(*pINFO)
+            print(banner)
+            playGame= input('Selection: ')
             try:
                 if int(playGame) == 1:
                     monster = monsterPicker(player=playerCard)
                     playerCard, monster = combat(playerCard, monster)
-                    # playerCard, monster = Save(playerCard.name).load_state()
+                    continue
                 elif int(playGame) == 2:
                     playerCard.heal_and_status()
                     continue
@@ -62,15 +67,21 @@ def main(playerCard:Player, monster:Monster):
             
 
         elif playerCard.status == "defeat":
+            sys('cls')
             print(playerCard)
-            playGame = int(input(f"""Looks like {monster.name} whoooped your ass. What are you going to do about that?
-                                                            
-                                                            {playerCard.hp}/{playerCard.baseHP}
-                                                            
-                                                            1.Heal
-                                                            2.Exit to Main Menu
-                                                            
-                                                        selection: """))
+            banner = '''You're now in the Hospital. You were defeated by {11}. What would you like to do?
+        
+        
+        LVL : {1}
+        HP  : {2}/{3}                           
+        ATK : {4}                               1. {9}
+        BLK : {5}                               2. {10}
+        STR : {6}
+        DFN : {7}
+        XP  : {12}/{13}
+                                                                                        '''.format(*pINFO)
+            print(banner)
+            playGame= input('Selection: ')
             try:
                 if int(playGame) == 1:
                     sys('cle')
